75f763231184936b6ed1c671616cd87d08dd4154
Angela Hempkmeier <angela.da.silva@avanade.com>
2023-12-07 19:33:22 -0300

    test(httpRequest): get request for the home page

--- cypress/e2e/APITesting/httpRequest.cy.js
+++ cypress/e2e/APITesting/httpRequest.cy.js
@@ -1,5 +1,7 @@
 describe("http request", () => {
     it("GET call", () => {
-        
+        cy.request('GET', 'https://bugbank.netlify.app/')
+        .its('status')
+        .should('equal', 200);
     })
 });
